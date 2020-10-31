2b9723e33c57a293720cee5819d24d5f47c36781
JasonMorgan <jmorgan@f9vs.com>
2020-10-31 13:23:37 -0400

    added some update repo steps.

--- ci/tasks/update-repo.sh
+++ ci/tasks/update-repo.sh
@@ -1,2 +1,14 @@
-#!/bin/bash
+#!/bin/env bash
 
+echo "$TBS_KUBECONFIG" | base64 -d > ~/config
+export KUBECONFIG=~/config
+
+NEW_IMAGE=$(kubectl get image "$IMAGE_NAME" -o json | jq '.status.latestImage' -r)
+PATTERN=$(echo "$NEW_IMAGE" | tr '@', "\n" | head -1)
+CONTENT=$(sed "s|image: $PATTERN.*|image: $NEW_IMAGE|" config-repo/"$DEPLOYMENT_MANIFEST")
+echo -n "$CONTENT" > config-repo/"$DEPLOYMENT_MANIFEST"
+
+pushd config-repo || exit 1
+git add "$DEPLOYMENT_MANIFEST"
+git commit -m "Manifest updated by concourse pipeline."
+popd || exit 1
