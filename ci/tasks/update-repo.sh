be4ab5473acc781e1ffdcd4d68f625391e6431ec
JasonMorgan <jmorgan@f9vs.com>
2020-10-31 13:26:05 -0400

    fixed a path.

--- ci/tasks/update-repo.sh
+++ ci/tasks/update-repo.sh
@@ -5,10 +5,10 @@ export KUBECONFIG=~/config
 
 NEW_IMAGE=$(kubectl get image "$IMAGE_NAME" -o json | jq '.status.latestImage' -r)
 PATTERN=$(echo "$NEW_IMAGE" | tr '@', "\n" | head -1)
-CONTENT=$(sed "s|image: $PATTERN.*|image: $NEW_IMAGE|" config-repo/"$DEPLOYMENT_MANIFEST")
-echo -n "$CONTENT" > config-repo/"$DEPLOYMENT_MANIFEST"
+CONTENT=$(sed "s|image: $PATTERN.*|image: $NEW_IMAGE|" cluster-repo/"$DEPLOYMENT_MANIFEST")
+echo -n "$CONTENT" > cluster-repo/"$DEPLOYMENT_MANIFEST"
 
-pushd config-repo || exit 1
+pushd cluster-repo || exit 1
 git add "$DEPLOYMENT_MANIFEST"
 git commit -m "Manifest updated by concourse pipeline."
 popd || exit 1
